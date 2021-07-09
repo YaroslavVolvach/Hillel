d4d1e40b72dededcac6ccf17b75f84a816db915e
YaroslavVolvach <60551329+YaroslavVolvach@users.noreply.github.com>
2021-07-09 10:11:29 +0300

    Create urls.py

---
+++ user/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from django.contrib.auth import views as auth_views
+from . import views
+from .forms import LoginForm
+
+app_name = 'user'
+
+urlpatterns = [
+    path('registration/', views.RegistrationView.as_view(), name='registration'),
+    path('login/', auth_views.LoginView.as_view(
+        authentication_form=LoginForm,
+        template_name='user/login.html',
+        redirect_authenticated_user='/'
+    ), name='login'),
+    path('logout/',  auth_views.LogoutView.as_view(), name='logout')
+    ]
